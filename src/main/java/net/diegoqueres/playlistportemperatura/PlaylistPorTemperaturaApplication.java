ac5a98e8b2295913d3d01077e9297c4a590b0754
Diego Queres <diegoqueres81@gmail.com>
2020-06-27 20:29:05 -0300

    Commit inicial.

---
+++ src/main/java/net/diegoqueres/playlistportemperatura/PlaylistPorTemperaturaApplication.java
@@ -0,0 +1,13 @@
+package net.diegoqueres.playlistportemperatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistPorTemperaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistPorTemperaturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/diegoqueres/playlistportemperatura/PlaylistPorTemperaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.diegoqueres.playlistportemperatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistPorTemperaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
